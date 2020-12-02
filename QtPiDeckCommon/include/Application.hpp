2024a2bb66ae1557f1a3341abb923cf1bd0a8dc6
Igor Sharapov <igorsharapov@outlook.com>
2020-12-02 10:31:27 +0300

    Remove boost-di

--- QtPiDeckCommon/include/Application.hpp
+++ QtPiDeckCommon/include/Application.hpp
@@ -1,28 +1,11 @@
 #pragma once
 
 #include "QtPiDeckCommon_global.hpp"
-#include <boost/di.hpp>
 
 #include <QQmlApplicationEngine>
 #include <QUrl>
 
 namespace QtPiDeck {
-template<class... TInjectors>
-class Ioc {
-public:
-    Ioc(TInjectors&&... injectors) : injector(boost::di::make_injector(injectors()...)) {
-    }
-
-    template<class T>
-    auto get() {
-        return injector.template create<T>();
-    }
-private:
-    decltype(boost::di::make_injector(std::declval<TInjectors&>()()...)) injector;
-};
-
-using EmptyIoc = Ioc<>;
-
 namespace detail {
 class QTPIDECKCOMMON_EXPORT Application {
 public:
@@ -39,10 +22,4 @@ protected:
 private:
 };
 }
-
-template <class Ioc = EmptyIoc>
-class Application : public detail::Application {
-  public:
-    static Ioc ioc;
-};
 }
