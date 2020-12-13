5c1ce59f8da9e822f724ca0fe3f436b10bea134b
Igor Sharapov <igorsharapov@outlook.com>
2020-12-13 15:34:15 +0300

    Add serialization operators (#22)
    * Add serialization operators
    
    * Add QDataStream overload for uint64_t if necessary

--- QtPiDeckCommon/include/Network/MessageHeader.hpp
+++ QtPiDeckCommon/include/Network/MessageHeader.hpp
@@ -2,15 +2,21 @@
 
 #include <cstdint>
 
+#include <QDataStream>
+
+#include "QtPiDeckCommon_global.hpp"
+
 namespace QtPiDeck::Network {
 enum class MessageId : uint32_t {
     Ping,
     Pong
 };
 
-//force 64bit alignment due to difference in arch of client and server
-struct alignas(64) MessageHeader {
+struct MessageHeader {
     uint64_t dataSize;
     MessageId messageId;
 };
+
+QTPIDECKCOMMON_EXPORT QDataStream& operator<<(QDataStream& str, MessageHeader & header);
+QTPIDECKCOMMON_EXPORT QDataStream& operator>>(QDataStream& str, MessageHeader & header);
 }
