48dbdddc306a8c4fe33cf1ed2aee60a5c4d5a03e
Igor Sharapov <igorsharapov@outlook.com>
2021-01-03 17:41:55 +0300

    Remove todo

--- QtChecker/include/QtVersion.hpp
+++ QtChecker/include/QtVersion.hpp
@@ -2,12 +2,9 @@
 
 #include <QtCore/qglobal.h>
 
-//TODO: same lines as in QtPiDeckCommon
-//      create a separate header-only lib?
 #ifndef QTPI4_VERSION
 #define QTPI4_VERSION QT_VERSION_CHECK(5, 11, 3)
 #endif
-//TODO: need to be renamed
 #ifndef QT_MIN_TESTED_VERSION
 #define QT_MIN_TESTED_VERSION QT_VERSION_CHECK(5, 15, 1)
 #endif
