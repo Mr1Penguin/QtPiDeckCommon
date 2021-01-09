c955710dd8c115518f532ac9e2b15712d7c3b244
Igor Sharapov <igorsharapov@outlook.com>
2021-01-09 16:55:56 +0300

    Add macros for qt6 style binding (#60)

---
+++ QtChecker/include/QtBinding.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <QtCore/qglobal.h>
+
+#if QT_VERSION_MAJOR == 5
+#define Q_OBJECT_BINDABLE_PROPERTY(CLASS, TYPE, NAME) TYPE NAME;
+#define BINDABLE(TYPE, NAME, PROPERTY_NAME)
+#else
+#define BINDABLE(TYPE, NAME, PROPERTY_NAME) QBindable<TYPE> NAME() { return &PROPERTY_NAME; }
+#endif
