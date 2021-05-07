122f2308daa90b5a0444f40eef1e5e322d56aff4
Igor Sharapov <igorsharapov@outlook.com>
2021-05-07 11:20:14 +0300

    Change include in main fail for test binary

--- Tests/QtPiDeckCommonCodeTests/src/main.cpp
+++ Tests/QtPiDeckCommonCodeTests/src/main.cpp
@@ -1,15 +1,3 @@
 #define BOOST_TEST_MODULE QtPiDeckCommonCodeTests
 #define BOOST_TEST_DYN_LINK
-//#define BOOST_TEST_NO_MAIN
-#include <boost/test/unit_test.hpp>
-/*
-#ifndef NDEBUG
-#include <qqmldebug.h>
-#endif
-
-int main(int argc, char* argv[], char* envp[]) {
-#ifndef NDEBUG
-  QQmlDebuggingEnabler enabler(false);
-#endif
-  return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
-}*/
+#include <boost/test/included/unit_test.hpp>
