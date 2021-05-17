24d1b96ca8740595dbf0cb294eab63a1ac4ddcb6
Igor Sharapov <igorsharapov@outlook.com>
2021-05-17 15:46:01 +0300

    Add clang-tidy macro for BOOST_FIXTURE_TEST_CASE (#75)

--- Tests/BoostTestWrapper/include/BoostUnitTest.hpp
+++ Tests/BoostTestWrapper/include/BoostUnitTest.hpp
@@ -12,3 +12,4 @@
 #define CT_BOOST_AUTO_TEST_SUITE_END() BOOST_AUTO_TEST_SUITE_END() // NOLINT
 #define CT_BOOST_AUTO_TEST_CASE(testcase) BOOST_AUTO_TEST_CASE(testcase) // NOLINT
 #define CT_BOOST_FIXTURE_TEST_SUITE(suite, fixture) BOOST_FIXTURE_TEST_SUITE(suite, fixture) // NOLINT
+#define CT_BOOST_FIXTURE_TEST_CASE(testcase, fixture) BOOST_FIXTURE_TEST_CASE(testcase, fixture) // NOLINT
