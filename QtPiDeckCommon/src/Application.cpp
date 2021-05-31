7243d25479eee2c94f832e6dca5ea08211a12f61
Igor Sharapov <igorsharapov@outlook.com>
2021-05-31 12:12:26 +0300

    Clean up Application.cpp

--- QtPiDeckCommon/src/Application.cpp
+++ QtPiDeckCommon/src/Application.cpp
@@ -3,56 +3,32 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#ifdef A1
-#error A1
-#endif
-
-#ifdef A2
-#error A2
-#endif
-
-#ifdef A3
-#error A3
-#endif
-
-#ifdef A4
-#error A4
-#endif
-
-#ifdef A5
-#error A5
-#endif
-
 namespace QtPiDeck {
-Application::Application() {
-    Application::s_current = this;
-}
+Application::Application() { Application::s_current = this; }
 
-auto Application::start(int argc, char **argv) -> int {
-    initialPreparations();
-    QGuiApplication app{argc, argv};
-    appCreated();
-    QQmlApplicationEngine engine;
-    engineCreated(engine);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, // clazy:exclude=connect-non-signal
-                     &app, [url = mainPage()](QObject *obj, const QUrl &objUrl) {
+auto Application::start(int argc, char** argv) -> int {
+  initialPreparations();
+  QGuiApplication app{argc, argv};
+  appCreated();
+  QQmlApplicationEngine engine;
+  engineCreated(engine);
+  auto pageUrl = mainPage();
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, // clazy:exclude=connect-non-signal
+      &app,
+      [&url = pageUrl](QObject* obj, const QUrl& objUrl) {
         if (obj == nullptr && url == objUrl) {
-            QCoreApplication::exit(-1);
+          QCoreApplication::exit(-1);
         }
-    }, Qt::QueuedConnection);
-    engine.load(mainPage());
-    return QGuiApplication::exec();
+      },
+      Qt::QueuedConnection);
+  engine.load(pageUrl);
+  return QGuiApplication::exec();
 }
 
-void Application::initialPreparations() {
+void Application::initialPreparations() {}
 
-}
+void Application::appCreated() {}
 
-void Application::appCreated() {
-
-}
-
-void Application::engineCreated(QQmlApplicationEngine & /*engine*/) {
-
-}
+void Application::engineCreated(QQmlApplicationEngine& /*engine*/) {}
 }
