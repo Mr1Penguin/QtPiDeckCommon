75b1e400c8ea662464b8d329055dbff030f43f4d
Igor Sharapov <igorsharapov@outlook.com>
2021-06-07 14:13:17 +0300

    Fix dep list usage and add cache calculation

--- scripts/vcpkgPackages.py
+++ scripts/vcpkgPackages.py
@@ -1,10 +1,5 @@
+from vcpkg.depList import getCommonList
 import sys
-
-#will be called from server/client script
-def getCommonList(args):
-    base = ["boost-test"]
-    add = {"qt6.1.0": "openssl"}
-    return base + [add[arg] for arg in filter(lambda x: x in add, args)]
 
 if __name__ == "__main__":
     res = getCommonList(sys.argv[1:])
