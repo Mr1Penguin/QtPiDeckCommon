1517c5e8ec7b8555fce25590ba5335a99d437116
Igor Sharapov <igorsharapov@outlook.com>
2021-06-13 11:07:11 +0300

    Update Qt6 to Qt6.1.1 (#102)

--- scripts/vcpkg/depList.py
+++ scripts/vcpkg/depList.py
@@ -1,5 +1,5 @@
 #will be called from server/client script
 def getCommonList(args):
     base = ["boost-test", "boost-log"]
-    add = {"qt6.1.0": "openssl"}
+    add = {"qt6.1.1": "openssl"}
     return base + [add[arg] for arg in filter(lambda x: x in add, args)]
