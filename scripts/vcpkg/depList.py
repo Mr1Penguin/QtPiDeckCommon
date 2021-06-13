cbced182ef05fb94aada102307bd8f189f1c1d0b
Igor Sharapov <igorsharapov@outlook.com>
2021-06-13 11:53:43 +0300

    Use list of packages for each key (#103)

--- scripts/vcpkg/depList.py
+++ scripts/vcpkg/depList.py
@@ -1,5 +1,7 @@
+from itertools import chain
+
 #will be called from server/client script
 def getCommonList(args):
     base = ["boost-test", "boost-log"]
-    add = {"qt6.1.1": "openssl"}
-    return base + [add[arg] for arg in filter(lambda x: x in add, args)]
+    add = {"qt6.1.1": ["openssl"]}
+    return chain(base, *[add[arg] for arg in filter(lambda x: x in add, args)])
