085ff9daeeb5b1e8f329d0d429851fbfb027b3be
Igor Sharapov <igorsharapov@outlook.com>
2021-06-20 12:59:51 +0300

    Suppress cland-tidy warning for qt macro

--- QtChecker/include/QtBinding.hpp
+++ QtChecker/include/QtBinding.hpp
@@ -3,7 +3,7 @@
 #include <QtCore/qglobal.h>
 
 #if QT_VERSION_MAJOR == 5
-#define Q_OBJECT_BINDABLE_PROPERTY(CLASS, TYPE, NAME, ...) TYPE NAME;
+#define Q_OBJECT_BINDABLE_PROPERTY(CLASS, TYPE, NAME, ...) TYPE NAME; // NOLINT(cppcoreguidelines-macro-usage)
 #define BINDABLE(TYPE, NAME, PROPERTY_NAME)
 #else
 #define BINDABLE(TYPE, NAME, PROPERTY_NAME) QBindable<TYPE> NAME() { return &PROPERTY_NAME; }
