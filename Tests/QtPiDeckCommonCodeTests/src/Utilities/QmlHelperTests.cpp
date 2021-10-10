6e4c806d49a186b032ea3f6275cab34d26f9442d
Igor Sharapov <igorsharapov@outlook.com>
2021-10-10 11:43:12 +0300

    Add tests for dp and sp functions

--- Tests/QtPiDeckCommonCodeTests/src/Utilities/QmlHelperTests.cpp
+++ Tests/QtPiDeckCommonCodeTests/src/Utilities/QmlHelperTests.cpp
@@ -20,7 +20,9 @@ public:
 };
 class EmptyScreen {
 public:
-  auto logicalDotsPerInch() const -> double { throw std::logic_error{"Should not be reachable"}; }
+  [[nodiscard]] auto logicalDotsPerInch() const -> qreal { // NOLINT(readability-convert-member-functions-to-static)
+    throw std::logic_error{"Should not be reachable"};     // LCOV_EXCL_LINE
+  }
 };
 class EmptyWindow {};
 
@@ -49,10 +51,10 @@ public:
 template<int64_t dpi>
 class Screen {
 public:
-  auto logicalDotsPerInch() const -> qreal { return static_cast<qreal>(dpi); }
+  [[nodiscard]] auto logicalDotsPerInch() const -> qreal { return static_cast<qreal>(dpi); }
 };
 
-constexpr uint64_t smallDpi = 60;
+constexpr uint64_t smallDpi   = 60;
 constexpr uint64_t regularDpi = 200;
 
 static_assert(smallDpi < regularDpi);
@@ -68,10 +70,29 @@ CT_BOOST_AUTO_TEST_CASE(dpiWithScreenAndSmallDpiShouldBeEqualToMinDpi) {
 }
 
 template<class Derived>
-using RegularDpiQmlHelper = QmlHelperPrivate<Derived, GuiAppWithScreen<Screen<regularDpi>>, EmptyWindow, Screen<regularDpi>>;
+using RegularDpiQmlHelper =
+    QmlHelperPrivate<Derived, GuiAppWithScreen<Screen<regularDpi>>, EmptyWindow, Screen<regularDpi>>;
 
 CT_BOOST_AUTO_TEST_CASE(dpiWithScreenAndRegularDpiShouldBeEqualToRegularDpi) {
   Helper<RegularDpiQmlHelper> helper;
   CT_BOOST_TEST(helper.dpi() == regularDpi);
+}
+
+using namespace boost::unit_test;
+
+const auto dpDataSet = data::make({0.0, static_cast<double>(Helper<NoScreenQmlHelper>::baseDpi),
+                                   static_cast<double>(regularDpi), 1000.0}) ^
+                       data::make({0.0, static_cast<double>(regularDpi), 250.0, 1250.0});
+
+CT_BOOST_DATA_TEST_CASE(dpShouldBeCalculatedCorrectly, dpDataSet, input, expected) {
+  Helper<RegularDpiQmlHelper> helper;
+  CT_BOOST_TEST(helper.dp(input, helper.dpi()) == expected);
+}
+
+const auto& spDataSet = dpDataSet; // same until font scale is used in sp(...) function
+
+CT_BOOST_DATA_TEST_CASE(spShouldBeCalculatedCorrectly, spDataSet, input, expected) {
+  Helper<RegularDpiQmlHelper> helper;
+  CT_BOOST_TEST(helper.sp(input, helper.dpi()) == expected);
 }
 CT_BOOST_AUTO_TEST_SUITE_END()
