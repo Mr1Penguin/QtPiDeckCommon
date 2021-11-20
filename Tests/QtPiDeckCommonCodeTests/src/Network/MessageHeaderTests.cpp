db2dd5df9dab7baab0b450e4a08dd07a70dd04c0
Igor Sharapov <igorsharapov@outlook.com>
2021-11-21 00:11:48 +0300

    Add tests for MessageHeader

--- Tests/QtPiDeckCommonCodeTests/src/Network/MessageHeaderTests.cpp
+++ Tests/QtPiDeckCommonCodeTests/src/Network/MessageHeaderTests.cpp
@@ -4,8 +4,9 @@
 #define BOOST_TEST_MODULE MessageHeaderTests
 #include "BoostUnitTest.hpp"
 
-#include "Network/MessageHeader.hpp"
 #include "Network/DeckDataStream.hpp"
+#include "Network/MessageHeader.hpp"
+#include "Utilities/Literals.hpp"
 
 #include "Utilities/Logging.hpp"
 
@@ -15,8 +16,13 @@ auto main(int argc, char* argv[]) -> int {
 }
 
 namespace QtPiDeck::Network {
+auto operator<<(std::ostream& ostr, const MessageType& right) -> std::ostream& {
+  ostr << static_cast<uint32_t>(right);
+  return ostr;
+}
+
 auto operator<<(std::ostream& ostr, const MessageHeader& right) -> std::ostream& {
-  ostr << "MessageHeader{ dataSize: " << right.dataSize << ", messageType: " << static_cast<uint32_t>(right.messageType)
+  ostr << "MessageHeader{ dataSize: " << right.dataSize << ", messageType: " << right.messageType
        << ", RequestId: " << right.requestId.toStdString().c_str() << " }";
   return ostr;
 }
@@ -28,9 +34,9 @@ auto operator==(const QtPiDeck::Network::MessageHeader& left, const QtPiDeck::Ne
 
 CT_BOOST_AUTO_TEST_SUITE(MessageHeaderTests)
 using namespace QtPiDeck::Network;
+using namespace QtPiDeck::Utilities;
 
-CT_BOOST_AUTO_TEST_CASE(ShouldBeSerializedAndDeserialized)
-{
+CT_BOOST_AUTO_TEST_CASE(shouldBeSerializedAndDeserialized) {
   const MessageHeader messageHeader{0, MessageType::Pong, QStringLiteral("a-random-id")};
   QByteArray qba;
   QDataStream in{&qba, DeckDataStream::OpenModeFlag::WriteOnly};
@@ -39,6 +45,15 @@ CT_BOOST_AUTO_TEST_CASE(ShouldBeSerializedAndDeserialized)
   MessageHeader outMessageHeader{};
   out >> outMessageHeader;
   CT_BOOST_TEST(outMessageHeader == messageHeader);
+}
+
+CT_BOOST_AUTO_TEST_CASE(shouldGetHeaderWithZeroDataSize) {
+  const auto messageType   = MessageType::Ping;
+  const auto requestId     = "PingId"_qs;
+  const auto messageHeader = getEmptyMessageHeader(messageType, requestId);
+  CT_BOOST_TEST(messageHeader.dataSize == 0);
+  CT_BOOST_TEST(messageHeader.messageType == messageType);
+  CT_BOOST_TEST(messageHeader.requestId == requestId);
 }
 
 CT_BOOST_AUTO_TEST_SUITE_END()
