8763b3d9dead0a1d3c25866bc9dc66fbe5a1b1f5
Igor Sharapov <igorsharapov@outlook.com>
2021-12-08 23:18:23 +0300

    Add bus message creation utility (#130)
    * Add bus message creation utility
    
    * Fix namespace of OpenMode
    
    * Remove extra ;
    
    * Add QDataStream header
    
    * Add QIODevice header
    
    * clang-tidy

--- QtPiDeckCommon/include/Bus/Message.hpp
+++ QtPiDeckCommon/include/Bus/Message.hpp
@@ -3,17 +3,37 @@
 #include <cstdint>
 
 #include <QByteArray>
+#include <QDataStream>
+#include <QIODevice>
 
 #include "Utilities/QBAutils.hpp"
 
 namespace QtPiDeck::Bus {
 struct Message {
-    const uint64_t messageType{};
-    const QByteArray payload{};
+  uint64_t messageType{};
+  QByteArray payload{};
 };
 
 template<class T>
 auto convertPayload(const Message& message) -> T {
   return Utilities::convert<T>(message.payload);
+}
+
+namespace detail {
+template<class TArg>    
+void addData(QDataStream& stream, TArg&& arg) {
+  stream << arg;
+}
+}
+
+template<class... TArgs>
+auto createMessage(uint64_t messageType, TArgs&&... args) -> Message {
+  auto message = Message{messageType, {}};
+  if constexpr (sizeof...(TArgs) != 0) {
+    auto stream = QDataStream{&message.payload, QIODevice::WriteOnly};
+    (detail::addData(stream, std::forward<TArgs>(args)), ...);
+  }
+
+  return message;
 }
 }
---
+++ Tests/QtPiDeckCommonCodeTests/src/Bus/MessageTests.cpp
@@ -0,0 +1,29 @@
+#include "BoostUnitTest.hpp"
+
+#include <QByteArray>
+#include <QString>
+
+#include "QtDefinitions.hpp"
+#include "Bus/Message.hpp"
+
+CT_BOOST_AUTO_TEST_SUITE(ConnectionTests)
+using namespace QtPiDeck::Bus;
+
+CT_BOOST_AUTO_TEST_CASE(createEmptyMessage) {
+  const auto type    = uint64_t{1};
+  const auto message = createMessage(type);
+  CT_BOOST_TEST(message.messageType == type);
+  CT_BOOST_TEST(message.payload.isEmpty());
+}
+
+CT_BOOST_AUTO_TEST_CASE(createMessageOneArgument) {
+  const auto message = createMessage(1, uint32_t{42});
+  CT_BOOST_TEST(message.payload.size() == sizeof(uint32_t));
+}
+
+CT_BOOST_AUTO_TEST_CASE(createMessageThreeArguments) {
+  const auto message = createMessage(1, uint32_t{42}, uint8_t{1}, CT_QStringLiteral("12"));
+  CT_BOOST_TEST(message.payload.size() == sizeof(uint32_t) + sizeof(uint8_t) + 8);
+}
+
+CT_BOOST_AUTO_TEST_SUITE_END()
