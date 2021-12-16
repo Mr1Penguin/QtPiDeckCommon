ce25d150ca2c362b9bb3b2db8b56e48f1cdda0c0
Igor Sharapov <igorsharapov@outlook.com>
2021-12-16 23:47:55 +0300

    clang-tidy

--- Tests/QtPiDeckCommonCodeTests/src/Utilities/OnExitTests.cpp
+++ Tests/QtPiDeckCommonCodeTests/src/Utilities/OnExitTests.cpp
@@ -1,19 +1,22 @@
 #include "BoostUnitTest.hpp"
 
+#include <map>
+
 #include "Utilities/OnExit.hpp"
 
 CT_BOOST_AUTO_TEST_SUITE(ConnectionTests)
 
 using namespace QtPiDeck::Utilities;
 
+constexpr auto newValue = 12;
+
 CT_BOOST_AUTO_TEST_CASE(CallOnScopeExit) {
   auto value             = 0;
   {
     auto scope = OnExit([&value] { ++value; });
-    value      = 12;
+    value      = newValue;
   }
 
   CT_BOOST_TEST(value == 13);
 }
-
 CT_BOOST_AUTO_TEST_SUITE_END()
