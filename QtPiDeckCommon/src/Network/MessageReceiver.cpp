2f02a19a223c8e92fe0758f845124c510102e289
Igor Sharapov <igorsharapov@outlook.com>
2022-05-21 22:10:29 +0300

    Receive json payload and delegate it to bus

--- QtPiDeckCommon/src/Network/MessageReceiver.cpp
+++ QtPiDeckCommon/src/Network/MessageReceiver.cpp
@@ -2,8 +2,10 @@
 
 #include <QJsonDocument>
 #include <cassert>
+#include <qjsondocument.h>
 #include <unordered_map>
 
+#include "Bus/Message.hpp"
 #include "Network/DeckDataStream.hpp"
 #include "Network/Messages.hpp"
 #include "Services/ISocketHolder.hpp"
@@ -18,5 +20,53 @@ MessageReceiver::MessageReceiver(dependency_list dependencies) {
   connect(service<Services::ISocketHolder>()->socket(), &QTcpSocket::readyRead, this, &MessageReceiver::readData);
 }
 
-void MessageReceiver::readData() {}
+namespace {
+auto readHeader(QIODevice& socket) -> std::optional<Network::MessageHeader> {
+  auto inStream = Network::DeckDataStream{&socket};
+  auto object   = Network::MessageHeader{};
+
+  inStream.startTransaction();
+  inStream >> object;
+  if (inStream.commitTransaction()) {
+    return object;
+  }
+
+  return std::nullopt;
+}
+}
+
+void MessageReceiver::readData() {
+  auto* socket       = service<Services::ISocketHolder>()->socket();
+  const auto& header = m_savedHeader.has_value() ? m_savedHeader : readHeader(*socket);
+  if (!header.has_value()) {
+    return;
+  }
+
+  auto payload = QJsonDocument{};
+  const auto hasPayload = header->dataSize > 0;
+  if (hasPayload) {
+    if (socket->bytesAvailable() < qint64{0} || static_cast<quint64>(socket->bytesAvailable()) < header->dataSize) {
+      if (!m_savedHeader.has_value()) {
+        m_savedHeader = header;
+      }
+
+      return;
+    }
+
+    auto inStream = Network::DeckDataStream{socket};
+    auto json = QString {};
+    inStream >> json;
+    payload = QJsonDocument::fromJson(json.toUtf8());
+    
+    m_savedHeader.reset();
+  }
+
+  const auto& messageTypeMapper = service<Services::IDeckMessageToBusMessageMapper>();
+  const auto busMessageType     = messageTypeMapper->getBusMessageType(header->messageType);
+  if (!busMessageType.has_value()) {
+    return;
+  }
+
+  service<Services::IMessageBus>()->sendMessage({*busMessageType, payload});
+}
 }
