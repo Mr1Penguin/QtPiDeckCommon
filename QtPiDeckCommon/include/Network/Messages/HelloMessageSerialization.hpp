d7430a7ba93e2a33fb48c8d15aac8a41fd1f4f2b
Igor Sharapov <igorsharapov@outlook.com>
2022-05-21 17:29:47 +0300

    Add tests for HelloSerialization

--- QtPiDeckCommon/include/Network/Messages/HelloMessageSerialization.hpp
+++ QtPiDeckCommon/include/Network/Messages/HelloMessageSerialization.hpp
@@ -1,10 +1,11 @@
-#include "HelloMessage.hpp"
+#include <QString>
 
+#include "HelloMessage.hpp"
 #include "QtPiDeckCommon_global.hpp"
 
 namespace QtPiDeck::Network {
 inline namespace Messages {
 auto QTPIDECKCOMMON_EXPORT prepareJsonString(const Hello& message) -> QString;
-auto QTPIDECKCOMMON_EXPORT prepareMessage(const QString& json) -> Hello;
+auto QTPIDECKCOMMON_EXPORT prepareHelloMessage(const QString& json) -> Hello;
 }
 }
