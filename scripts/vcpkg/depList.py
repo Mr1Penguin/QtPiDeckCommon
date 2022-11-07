3135e681c4572e179624dfb2506276b99febe602
Igor Sharapov <igorsharapov@outlook.com>
2022-11-07 14:39:03 +0200

    Try CMake presets

--- scripts/vcpkg/depList.py
+++
@@ -1,7 +0,0 @@
-from itertools import chain
-
-#will be called from server/client script
-def getCommonList(args):
-    base = ["boost-test", "boost-log"]
-    add = {"qt6.4.0": ["openssl"]}
-    return chain(base, *[add[arg] for arg in filter(lambda x: x in add, args)])
