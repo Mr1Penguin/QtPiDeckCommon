655d08736eda670e57dfcd65ca229bf6742e871c
Igor Sharapov <igorsharapov@outlook.com>
2022-11-07 09:12:06 +0200

    Fix variable name

--- scripts/vcpkg/depList.py
+++ scripts/vcpkg/depList.py
@@ -3,5 +3,5 @@ from itertools import chain
 #will be called from server/client script
 def getCommonList(args):
     base = ["boost-test", "boost-log"]
-    add = {"qt6.2.1": ["openssl"]}
+    add = {"qt6.4.0": ["openssl"]}
     return chain(base, *[add[arg] for arg in filter(lambda x: x in add, args)])
